52d8a8161727519316262ae875b5071bba533e19
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-12-21 15:07:11 -0800

    Huge Update
    added posts,
    added database
    added admins

--- django_project/social_media/views.py
+++ django_project/social_media/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': "ArmenG",
-        'title': 'My photo',
-        'content': 'bwoah',
-        'data_posted':'June 10, 2777'
-    }
-]
-
-
+from .models import Post
 def home(request):
     context = {
-        'posts':posts
+        'posts': Post.objects.all()
     }
-    return render(request, 'social_media/home.html',context)
+    return render(request, 'social_media/home.html', context)
+
     
